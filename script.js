902fd3333b033cd48af1be8449b78630bebdc243
Jennifer Wang <jennifer123wang@gmail.com>
2020-07-28 20:25:25 -0400

    sound changes

--- script.js
+++ script.js
@@ -12,49 +12,85 @@ chrome.runtime.onMessage.addListener(
     }
 );
 
+let label;
 
-let capture;
+let control;
+let isActive;
+
+
+// let capture;
+
+// function setup() {
+//   createCanvas(300, 300);
+//   capture = createCapture(VIDEO);
+//   capture.size(320, 240);
+//   //capture.hide();
+// }
+
+// function draw() {
+//   background(255);
+//   image(capture, 300, 300, 320, 240);
+// }
+
+// let isNewWindow = false;
+// // Classifier Variable
+// let classifier;
+// // Model URL
+// let imageModelURL = 'https://teachablemachine.withgoogle.com/models/CJ7DXQnyz/';
+
+// // Video
+// let video;
+// let flippedVideo;
+// // To store the classification
+// let label = "";
+
+// // Load the model first
+// function preload() {
+// classifier = ml5.imageClassifier(imageModelURL + 'model.json');
+// }
+
+// function setup() {
+// createCanvas(320, 260);
+// // Create the video
+// video = createCapture(VIDEO);
+// video.size(320, 240);
+// video.hide();
+
+// flippedVideo = ml5.flipImage(video);
+// // Start classifying
+// classifyVideo();
+// }
+
+var myAudio = new Audio();
 
 function setup() {
-  createCanvas(300, 300);
-  capture = createCapture(VIDEO);
-  capture.size(320, 240);
-  //capture.hide();
+    myAudio.src = chrome.runtime.getURL("song.mp3");
+    control = new Control();
+    //looper.start();
+    isActive = true;
+
+    if (isActive) {
+        checkCondition();
+        control.display();
+    }
+}   
+
+function checkCondition() {
+    //modify this to check if touching face
+    // if (collidePointCircle(mouseX, mouseY, width/2, height/2, 100)) { 
+    if (updateLabel()) {
+      control.setTrue();
+    }
+    else {
+      control.setFalse();
+      
+    }
 }
 
-function draw() {
-  background(255);
-  image(capture, 300, 300, 320, 240);
+function updateLabel() {
+    return label == 'Touching';
 }
 
-let isNewWindow = false;
-// Classifier Variable
-let classifier;
-// Model URL
-let imageModelURL = 'https://teachablemachine.withgoogle.com/models/CJ7DXQnyz/';
-
-// Video
-let video;
-let flippedVideo;
-// To store the classification
-let label = "";
-
-// Load the model first
-function preload() {
-classifier = ml5.imageClassifier(imageModelURL + 'model.json');
-}
-
-function setup() {
-createCanvas(320, 260);
-// Create the video
-video = createCapture(VIDEO);
-video.size(320, 240);
-video.hide();
-
-flippedVideo = ml5.flipImage(video);
-// Start classifying
-classifyVideo();
-}
 
 function draw() {
 background(0);
@@ -68,11 +104,12 @@ textAlign(CENTER);
 text(label, width / 2, height - 4);
 }
 
+
 // Get a prediction for the current video frame
 function classifyVideo() {
-flippedVideo = ml5.flipImage(video)
-classifier.classify(flippedVideo, gotResult);
-flippedVideo.remove();
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
 
 }
 
@@ -106,3 +143,37 @@ chrome.runtime.onMessage.addListener(
             
     });
 
+
+    class Control {
+        constructor() {
+          this.isTouched = false;
+        }
+        getBool() {
+          return this.isTouched;
+        }
+        setTrue() {
+          this.isTouched = true;
+        }
+        setFalse() {
+          this.isTouched = false;
+        }
+        display() {
+          if (this.isTouched) {
+            this.displayTrue();
+            // myAudio.play();
+          }
+          else {
+            this.displayFalse();
+          }
+        }
+        //modify these methods
+        displayTrue() {
+          text('stop touching face!', 50, 50);
+          myAudio.play();
+        }
+        displayFalse() {
+          text('nice job :)', 50, 50);
+          myAudio.pause();
+          myAudio.currentTime = 0;
+        }
+    }
